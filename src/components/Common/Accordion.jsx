51fd5203695868751f473b18bf8fe9a9d795937e
Sabita Sitaula <sabitasitaula705@gmail.com>
2022-05-01 00:21:49 +0545

    fixed accordion

--- src/components/Common/Accordion.jsx
+++ src/components/Common/Accordion.jsx
@@ -7,7 +7,7 @@ const Accordion = ({ header1, description1, header2, description2 }) => {
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingOne">
             <button
-              name="accordion-button"
+              className="accordion-button"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#collapseOne"
